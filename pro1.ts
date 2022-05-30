4ae32910cc701e81a8137ba0e9b9d1c226f1cd70
Longinus44 <chijiokelonginus4@gmail.com>
2022-05-30 11:15:51 +0000

    my work

---
+++ pro1.ts
@@ -0,0 +1,11 @@
+// let digit = document.getElementById('input').value
+// let number: number;
+// number = 0;
+// function cross(){
+// // for(let i = 1; i <= digit; i++)
+// if(digit % 2 == 0)
+
+// }
+
+
+
